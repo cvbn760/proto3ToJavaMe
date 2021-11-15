103c0d5123f4137ca82999a492dde987e9c10feb
cvbn760 <ross31456@gmail.com>
2021-11-15 16:20:44 +0300

    fix
    -в генерируемых классах убраны методы, которые не будут задействованны

--- src/content/businnes/sourcebuilder/instancemethods/InstanceMethodsBuilderImpl.java
+++ src/content/businnes/sourcebuilder/instancemethods/InstanceMethodsBuilderImpl.java
@@ -1,13 +1,11 @@
 package content.businnes.sourcebuilder.instancemethods;
 
 
-
 import content.businnes.sourcebuilder.resource.JavaSourceCodeUtil;
 import content.businnes.sourcebuilder.resource.ResourceFormatUtil;
 import content.domain.metadata.DataType;
 import content.domain.proto.FieldData;
 import content.domain.proto.ProtoFileInput;
-import content.domain.proto.ValidScopes;
 import content.domain.proto.ValidTypes;
 
 import java.util.Iterator;
@@ -23,58 +21,8 @@ public final class InstanceMethodsBuilderImpl implements InstanceMethodsBuilder 
     public StringBuilder createPrivateAndProtectedMethods(String className, ProtoFileInput protoInput) {
         this.protoInput = protoInput;
         StringBuilder builder = new StringBuilder();
-        builder.append(this.createComputeSize()); // Добавляет метод посчета размера
-        builder.append(this.createComputeSizeNestedMessages()); //
-        builder.append(this.createWriteFields()); //
-        builder.append(this.createParseFields(className)); //
-        builder.append(this.createGetNextFieldNumber()); //
-        builder.append(this.createPopulateWithField(className)); //
+        builder.append(this.createPopulateWithField(className));
         return builder;
-    }
-
-    private String createComputeSize() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(this.resourceFormat.getString("public.compute.size.start"));
-        Iterator i$ = this.protoInput.getFields().iterator();
-
-        while (i$.hasNext()) {
-            FieldData field = (FieldData) i$.next();
-            if (this.isValidType(field.getType())) {
-                if (field.isList()) {
-                    builder.append(this.resourceFormat.getString("public.compute.size.fields.list", ResourceFormatUtil.COMMON.getString("class.compute.size.util"), JavaSourceCodeUtil.createCapitalLetterMethod(field.getListImpl().getImplementation()), JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), ResourceFormatUtil.COMMON.getString("class.supported.data.types"), field.getType().getDataTypeConstant(), field.getName()));
-                } else if (field.getScope() == ValidScopes.REQUIRED) {
-                    builder.append(this.resourceFormat.getString("public.compute.size.fields", ResourceFormatUtil.COMMON.getString("class.compute.size.util"), JavaSourceCodeUtil.createCapitalLetterMethod(field.getType().getImplementationType()), JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), field.getName()));
-                } else {
-                    builder.append(this.resourceFormat.getString("public.compute.size.fields.optional", JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), ResourceFormatUtil.COMMON.getString("class.compute.size.util"), JavaSourceCodeUtil.createCapitalLetterMethod(field.getType().getImplementationType()), JavaSourceCodeUtil.createFieldNumberName(field.getName()), field.getName()));
-                }
-            }
-        }
-
-        builder.append(this.resourceFormat.getString("public.compute.size.fields.nested"));
-        builder.append(this.resourceFormat.getString("public.compute.size.end"));
-        return builder.toString();
-    }
-
-    private String createComputeSizeNestedMessages() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(this.resourceFormat.getString("private.compute.message.size.start"));
-        Iterator i$ = this.protoInput.getFields().iterator();
-
-        while (i$.hasNext()) {
-            FieldData field = (FieldData) i$.next();
-            if (!this.isValidType(field.getType())) {
-                if (field.getScope() == ValidScopes.REQUIRED) {
-                    builder.append(this.resourceFormat.getString("private.compute.message.size.custom.type", ResourceFormatUtil.COMMON.getString("class.compute.size.util"), JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), field.getName()));
-                } else if (field.isList()) {
-                    builder.append(this.resourceFormat.getString("private.compute.message.size.custom.type.list", ResourceFormatUtil.COMMON.getString("class.compute.size.util"), JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), ResourceFormatUtil.COMMON.getString("class.supported.data.types"), field.getName()));
-                } else {
-                    builder.append(this.resourceFormat.getString("private.compute.message.size.custom.type.optional", JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), ResourceFormatUtil.COMMON.getString("class.compute.size.util"), JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), field.getName()));
-                }
-            }
-        }
-
-        builder.append(this.resourceFormat.getString("private.compute.message.size.end"));
-        return builder.toString();
     }
 
     private boolean isValidType(DataType type) {
@@ -91,44 +39,6 @@ public final class InstanceMethodsBuilderImpl implements InstanceMethodsBuilder 
         }
 
         return isValidType;
-    }
-
-    private String createWriteFields() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(this.resourceFormat.getString("public.writefields.start", ResourceFormatUtil.COMMON.getString("class.output.writer")));
-        Iterator i$ = this.protoInput.getFields().iterator();
-
-        while (i$.hasNext()) {
-            FieldData field = (FieldData) i$.next();
-            if (field.isList()) {
-                if (this.isValidType(field.getType())) {
-                    builder.append(this.resourceFormat.getString("public.writefields.fields.list", JavaSourceCodeUtil.createCapitalLetterMethod(field.getListImpl().getImplementation()), JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), ResourceFormatUtil.COMMON.getString("class.supported.data.types"), field.getType().getDataTypeConstant(), field.getName()));
-                } else {
-                    builder.append(this.resourceFormat.getString("public.writefields.fields.list.nested", JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), ResourceFormatUtil.COMMON.getString("class.supported.data.types"), field.getName()));
-                }
-            } else if (field.getScope() == ValidScopes.REQUIRED) {
-                if (this.isValidType(field.getType())) {
-                    builder.append(this.resourceFormat.getString("public.writefields.fields", JavaSourceCodeUtil.createCapitalLetterMethod(field.getType().getImplementationType()), JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), field.getName()));
-                } else {
-                    builder.append(this.resourceFormat.getString("public.writefields.fields.nested", JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), field.getName()));
-                }
-            } else if (this.isValidType(field.getType())) {
-                builder.append(this.resourceFormat.getString("public.writefields.fields.optional", JavaSourceCodeUtil.createFieldNumberName(field.getName()), JavaSourceCodeUtil.createCapitalLetterMethod(field.getType().getImplementationType()), field.getName(), JavaSourceCodeUtil.createCapitalLetterMethod(field.getName())));
-            } else {
-                builder.append(this.resourceFormat.getString("public.writefields.fields.optional.nested", JavaSourceCodeUtil.createCapitalLetterMethod(field.getName()), field.getName()));
-            }
-        }
-
-        builder.append(this.resourceFormat.getString("public.writefields.end"));
-        return builder.toString();
-    }
-
-    private String createParseFields(String className) {
-        return this.resourceFormat.getString("packageprotected.static.parsefields", className, ResourceFormatUtil.COMMON.getString("class.input.reader"));
-    }
-
-    private String createGetNextFieldNumber() {
-        return this.resourceFormat.getString("packageprotected.static.getnextfieldnumber", ResourceFormatUtil.COMMON.getString("class.input.reader"));
     }
 
     private String createPopulateWithField(String className) {
--- src/content/businnes/sourcebuilder/staticmethods/StaticMethodsBuilderImpl.java
+++ src/content/businnes/sourcebuilder/staticmethods/StaticMethodsBuilderImpl.java
@@ -11,24 +11,24 @@ public final class StaticMethodsBuilderImpl implements StaticMethodsBuilder {
 
     public StringBuilder createStaticMethods(String className) {
         StringBuilder builder = new StringBuilder();
-        builder.append(this.createParseFromByteArray(className));
-        builder.append(this.createParseFromInputStream(className));
-        builder.append(this.createParseDelimitedFromInputStream(className));
+//        builder.append(this.createParseFromByteArray(className));
+//        builder.append(this.createParseFromInputStream(className));
+//        builder.append(this.createParseDelimitedFromInputStream(className));
         builder.append(this.createClassEnd());
         return builder;
     }
 
-    private String createParseFromByteArray(String className) {
-        return this.resourceFormat.getString("static.parsefrom.bytearray", className);
-    }
+//    private String createParseFromByteArray(String className) {
+//        return this.resourceFormat.getString("static.parsefrom.bytearray", className);
+//    }
 
-    private String createParseFromInputStream(String className) {
-        return this.resourceFormat.getString("static.parsefrom.inputstream", className);
-    }
+//    private String createParseFromInputStream(String className) {
+//        return this.resourceFormat.getString("static.parsefrom.inputstream", className);
+//    }
 
-    private String createParseDelimitedFromInputStream(String className) {
-        return this.resourceFormat.getString("static.parsedelimitedfrom.inputstream", className);
-    }
+//    private String createParseDelimitedFromInputStream(String className) {
+//        return this.resourceFormat.getString("static.parsedelimitedfrom.inputstream", className);
+//    }
 
     private String createClassEnd() {
         return this.resourceFormat.getString("class.end");
